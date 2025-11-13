5375e6b6e763e7f96d4b97530ec7e84a5fbf93d3
Kauancpi <kauancpi@gmail.com>
2025-11-13 15:47:25 -0300

    Tentando fazer uma funcao pra enviar email

--- stock-alert/Program.cs
+++ stock-alert/Program.cs
@@ -2,13 +2,35 @@
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+using MailKit.Security;
 
 namespace stock_alert
 {
     class Program
     {
+        //Funcao pra mandar um email
+        public static void SendEmail(string email, string subject, string messageBody)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("stockalertkauan", "stockalertkauan@gmail.com"));
+            message.To.Add(new MailboxAddress("", email));
+            message.Subject = subject;
+            message.Body = new TextPart("plain") { Text = messageBody };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 465, SecureSocketOptions.SslOnConnect);
+                client.Authenticate("stockalertkauan@gmail.com", "kcci zdov vtju wygu");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
         static async Task Main()
         {
+            //Parte do código que recebe o preço da API
             string token = "eoBmtRMk6mhUioq9hqrGtM";
             string ticker = "PETR4";
             string url = $"https://brapi.dev/api/quote/{ticker}?token={token}";
@@ -23,6 +45,10 @@ namespace stock_alert
 
             float currentprice = float.Parse(preco,System.Globalization.CultureInfo.InvariantCulture);
 
-        }
+            
+
+            SendEmail("kauancpi@gmail.com","Teste","teste");
+            }
+            
     }
 }
