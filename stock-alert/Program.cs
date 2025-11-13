afc81b65f10a5917858ba014b1a889682ec14bbd
Kauancpi <kauancpi@gmail.com>
2025-11-13 12:50:37 -0300

    getting the price

--- stock-alert/Program.cs
+++ stock-alert/Program.cs
@@ -5,47 +5,24 @@ using System.Threading.Tasks;
 
 namespace stock_alert
 {
-    public class Quote
+    class Program
     {
-        public string Symbol { get; set; }
-        public string ShortName { get; set; }
-        public decimal RegularMarketPrice { get; set; }
-        public decimal RegularMarketChangePercent { get; set; }
-        public string Currency { get; set; }
-    }
-    public class QuoteResponse
-    {
-        public Quote[] Results { get; set; }
-    }
-    public class BrapiClient
-    {
-        private readonly HttpClient _httpClient;
-        private readonly string _token;
-        private const string BaseUrl = "https://brapi.dev/api";
-        public BrapiClient(string token)
-        {
-            _token = token;
-            _httpClient = new HttpClient
-            {
-                Timeout = TimeSpan.FromSeconds(10)
-            };
-        }
-        public async Task<Quote> GetQuoteAsync(string ticker)
-        {
-            var url = $"{BaseUrl}/quote/{ticker}?token={_token}";
-            var response = await _httpClient.GetStringAsync(url);
-            var data = JsonSerializer.Deserialize<QuoteResponse>(response);
-            return data?.Results?[0];
-        }
         static async Task Main()
         {
-            var client = new BrapiClient("SEU_TOKEN");
-            var quote = await client.GetQuoteAsync("PETR4");
+            string token = "eoBmtRMk6mhUioq9hqrGtM";
+            string ticker = "PETR4";
+            string url = $"https://brapi.dev/api/quote/{ticker}?token={token}";
+            using HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(url);
 
-            if (quote != null)
-            {
-                Console.WriteLine($"{quote.Symbol}: R$ {quote.RegularMarketPrice:F2}");
-            }
+            int precopos1 = response.IndexOf("\"regularMarketPrice\":");
+            int precopos2 = response.IndexOf(",\"regularMarketDayHigh\":");
+            
+            string preco = response.Substring(precopos1+21,precopos2-precopos1-21);
+
+
+            float currentprice = float.Parse(preco,System.Globalization.CultureInfo.InvariantCulture);
+
         }
     }
 }
